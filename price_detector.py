11d963cfca5112ae7e3d357cc0526c66b150583a
Ghanshyam Didwaniya <97013366+BestBabaJii@users.noreply.github.com>
2023-06-13 17:05:46 +0530

    Add files via upload

--- price_detector.py
+++ price_detector.py
@@ -1,6 +1,11 @@
 import pandas
-import matplotlib.pyplot as plt 
+from sklearn.linear_model import LinearRegression
+
+
 data = pandas.read_csv('iphone_price.csv')
 # print(data.head())
-plt.scatter(data['version'],data['price'])
-plt.show()
+
+model = LinearRegression()
+model.fit(data[['version']],data[['price']])
+
+# model.pridect([[14]])
