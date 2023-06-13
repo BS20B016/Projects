bf59691853e0809838c94ba2bb5c1668d4413cc1
Ghanshyam Didwaniya <97013366+BestBabaJii@users.noreply.github.com>
2023-06-13 17:05:00 +0530

    Add files via upload

--- price_detector.py
+++ price_detector.py
@@ -1,3 +1,6 @@
-import pandas 
+import pandas
+import matplotlib.pyplot as plt 
 data = pandas.read_csv('iphone_price.csv')
-print(data.head())
+# print(data.head())
+plt.scatter(data['version'],data['price'])
+plt.show()
